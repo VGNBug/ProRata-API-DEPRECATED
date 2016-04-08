9a2da4e4d86b07b6cde9be5886597fcb3394110f
Daniel Pawsey <daniel.pawsey@live.co.uk>
2016-04-08 09:26:28 +0100

    Update ApplicationTests.java

--- src/test/java/com/prorata/ApplicationTests.java
+++ src/test/java/com/prorata/ApplicationTests.java
@@ -9,13 +9,13 @@ import com.prorata.Application;
 import org.springframework.boot.test.SpringApplicationConfiguration;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 
-// @RunWith(SpringJUnit4ClassRunner.class)
-// @SpringApplicationConfiguration(classes = Application.class)
-// @WebAppConfiguration
+@RunWith(SpringJUnit4ClassRunner.class)
+@SpringApplicationConfiguration(classes = Application.class)
+@WebAppConfiguration
 public class ApplicationTests {
 
-	// @Test
-	// public void contextLoads() {
-	// }
+	@Test
+	public void contextLoads() {
+	}
 
 }
